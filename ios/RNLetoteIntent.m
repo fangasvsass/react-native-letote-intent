d4fad8ec61b8719ef53caeffc1e64eb92baf4239
Blake <fffffangqi@gmail.com>
2018-05-10 13:20:43 +0800

    Update RNLetoteIntent.m

--- ios/RNLetoteIntent.m
+++ ios/RNLetoteIntent.m
@@ -1,5 +1,7 @@
-
 #import "RNLetoteIntent.h"
+#import <UIKit/UIKit.h>
+
+#define EXCEED_IOS_10 ([[[UIDevice currentDevice] systemVersion] floatValue]>= 10.0 ? YES : NO)
 
 @implementation RNLetoteIntent
 
@@ -9,5 +11,54 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_EXPORT_METHOD(gotoPermissionSetting) {
+//    NSURL * url= [NSURL URLWithString:@"prefs:root=LOCATION_SERVICES"];
+    NSURL * url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+    if (EXCEED_IOS_10) {
+        if ([[UIApplication sharedApplication] canOpenURL:url]) {
+            [[UIApplication sharedApplication]openURL:url options:@{} completionHandler:^(BOOL success) {
+            }];
+        }
+    }else{
+        if ([[UIApplication sharedApplication] canOpenURL:url]) {
+            [[UIApplication sharedApplication] openURL:url];
+        }
+    }
+}
+/*
+ About — prefs:root=General&path=About
+ Accessibility — prefs:root=General&path=ACCESSIBILITY
+ AirplaneModeOn— prefs:root=AIRPLANE_MODE
+ Auto-Lock — prefs:root=General&path=AUTOLOCK
+ Brightness — prefs:root=Brightness
+ Bluetooth — prefs:root=General&path=Bluetooth
+ Date& Time — prefs:root=General&path=DATE_AND_TIME
+ FaceTime — prefs:root=FACETIME
+ General— prefs:root=General
+ Keyboard — prefs:root=General&path=Keyboard
+ iCloud — prefs:root=CASTLE  iCloud
+ Storage & Backup — prefs:root=CASTLE&path=STORAGE_AND_BACKUP
+ International — prefs:root=General&path=INTERNATIONAL
+ Location Services — prefs:root=LOCATION_SERVICES
+ Music — prefs:root=MUSIC
+ Music Equalizer — prefs:root=MUSIC&path=EQ
+ Music VolumeLimit— prefs:root=MUSIC&path=VolumeLimit
+ Network — prefs:root=General&path=Network
+ Nike + iPod — prefs:root=NIKE_PLUS_IPOD
+ Notes — prefs:root=NOTES
+ Notification — prefs:root=NOTIFICATIONS_ID
+ Phone — prefs:root=Phone
+ Photos — prefs:root=Photos
+ Profile — prefs:root=General&path=ManagedConfigurationList
+ Reset — prefs:root=General&path=Reset
+ Safari — prefs:root=Safari  Siri — prefs:root=General&path=Assistant
+ Sounds — prefs:root=Sounds
+ SoftwareUpdate— prefs:root=General&path=SOFTWARE_UPDATE_LINK
+ Store — prefs:root=STORE
+ Twitter — prefs:root=TWITTER
+ Usage — prefs:root=General&path=USAGE
+ VPN — prefs:root=General&path=Network/VPN
+ Wallpaper — prefs:root=Wallpaper
+ Wi-Fi — prefs:root=WIFI
+ */
 @end
-  
